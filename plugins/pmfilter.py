0fe97f4f5a391150d65ab9626463e0a816597f07
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2025-09-22 22:39:41 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2569,16 +2569,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
         ]
             reply_markup = InlineKeyboardMarkup(buttons)
-            await query.message.edit_text(
-                text="● ◌ ◌"
-            )
-            await query.message.edit_text(
-                text="● ● ◌"
-            )
-            await query.message.edit_text(
-                text="● ● ●"
-            )
-            reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
     await query.answer(MSG_ALRT)
 
