393d832fcf95472fd827e3f1dcd1c03c7f10f17d
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2025-10-24 09:07:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.j
 MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/G1j.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/242b7f1b52743938d81f1.jpg'))
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg')).split()  # Fsub pic
 DAILY_LIMIT = int(environ.get("DAILY_LIMIT", "20")) 
 # ============================
 # Admin, Channels & Users Configuration
